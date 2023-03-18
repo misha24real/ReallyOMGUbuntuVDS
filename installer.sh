347d8d791ed87fa43ca5c10bb1b5118440fe1459
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-18 20:47:15 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -92,18 +92,20 @@ echo -e ""
 echo -e "${AQUA}${BLACK}Script Developed by Niksss#5489"
 ./proot -S . -w /root /usr/bin/env -i MOZ_FAKE_NO_SANDBOX=1 HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=xterm LANG=en_US.UTF-8 LC_ALL=C LANGUAGE=en_US /bin/bash --login
 else
-dir=$HOME
-keep_file_1=./server.jar
-keep_file_2=./ip
-keep_file_3=./port
-keep_file_4=./sysinstall
-keep_file_5=./key.txt
-for file in $dir/*
+directory=$HOME
+keep_files=("./server.jar" "./key.txt" "./ip" "./port" "./sysinstall")
+for file in $directory/*
 do
-    if [[ "$file" != "$keep_file_1" || "$file" != "$keep_file_2" || "$file" != "$keep_file_3" || "$file" != "$keep_file_4" || "$file" != "$keep_file_5" ]]
-    then
-        rm -fr $file
-    fi
+    delete=1
+    for keeped_file in $keep_files
+    do
+        if [[ "$file" == "$keeped_file" ]]
+        then
+            delete=0
+            break
+        fi
+    done
+    [ $delete -eq 1 ] && rm -fr $file
 done
 echo "Скачивание системы... (0%)"
 curl -sSLo root.tar.gz https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Ubuntu20/focal-amd64.tar.gz
